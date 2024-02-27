f8857a6ea1340e3c34edde0b14bffeaf9d0dbeb7
somotto <stephenonyango4783@gmail.com>
2024-02-27 19:58:41 +0300

    myfamily

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-curl -s https://learn.zone01kisumu.ke/assets/superhero/all.json | jq -r 'map(select(.id == '$HERO_ID'))[0] | .connections.relatives'
+curl -s https://learn.zone01kisumu.ke/assets/superhero/all.json | jq -r 'map(select(.id == '$HERO_ID'))[0] | .connections.relatives | sub ("\n";"\\n")'
