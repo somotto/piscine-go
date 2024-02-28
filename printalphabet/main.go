2e40a90c627020b6f876e732ca5f440c30e84818
somotto <stephenonyango4783@gmail.com>
2024-02-28 19:28:18 +0300

    Addition

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,9 +3,9 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	var x rune = 97
-	for i := x; i <= 122; i++ {
+	var x rune = 122
+	for i := x; i <= 97; i++ {
 		z01.PrintRune(i)
 	}
-	z01.PrintRune(10)
+	z01.PrintRune('\n')
 }
