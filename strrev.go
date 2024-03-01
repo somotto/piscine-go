6e0dbce3df869d640cd372a04c4114a5f25204e1
somotto <stephenonyango4783@gmail.com>
2024-03-01 21:06:15 +0300

    strv

--- strrev.go
+++ strrev.go
@@ -1,12 +1,9 @@
 package piscine
 
 func StrRev(s string) string {
-	r := []rune(s)
-	length := len(s)
-	var revRun []rune
-
-	for i := length - 1; i >= 0; i-- {
-		revRun = appen(revRun, r[i])
+	var reveservedString string
+	for i := len(s) - 1; i >= 0; i-- {
+		reveservedString += string(s[i])
 	}
-	return string(revRun)
+	return reveservedString
 }
