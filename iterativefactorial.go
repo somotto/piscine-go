0480cfdf70741623f0bd434d460c287683381b11
somotto <stephenonyango4783@gmail.com>
2024-03-04 14:17:01 +0300

    itfact

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,7 +3,7 @@ package piscine
 func IterativeFactorial(nb int) int {
 	if nb > 0 && nb <= 20 {
 		result := 1
-		for i := 1; i <= nb; i++ {
+		for i := 2; i <= nb; i++ {
 			result *= i
 			return result
 		}
