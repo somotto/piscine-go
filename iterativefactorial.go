8b05dcf817e44a5e82cd8eeebf992ef44ff9f782
somotto <stephenonyango4783@gmail.com>
2024-03-04 14:02:38 +0300

    itfact

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -5,10 +5,7 @@ func IterativeFactorial(nb int) int {
 	if nb < 0 {
 		return 0
 	}
-	if nb == 0 || nb == 1 {
-		return 1
-	}
-	for i := 2; i <= nb; i++ {
+	for i := 1; i <= nb; i++ {
 		if result > (1 << 31) {
 			return 0
 		}
