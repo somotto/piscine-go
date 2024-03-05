0d506209a6b5e97ce60165c54b690668d8d68dd7
somotto <stephenonyango4783@gmail.com>
2024-03-05 16:42:32 +0300

    findnt

--- findnextprime.go
+++ findnextprime.go
@@ -1,19 +1,14 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-	if nb <= 1 {
-		return 2
-	}
-	if nb%2 == 0 {
-		nb++
-	}
-	for i := nb + 2; i < nb/2; i += 2 {
-		if IsPrime(i) {
-			return i
-		}
-	}
 	if IsPrime(nb) {
 		return nb
+	}
+	for i := nb; i <= nb+5; i++ {
+		nb++
+		if IsPrime(nb) {
+			return nb
+		}
 	}
 	return 0
 }
