c98f3c3e1e6d27f0cb57b298ceabd934c1c5965e
somotto <stephenonyango4783@gmail.com>
2024-03-07 09:54:08 +0300

    isalp

--- isalpha.go
+++ isalpha.go
@@ -4,10 +4,11 @@ func IsAlpha(s string) bool {
 	if len(s) == 0 {
 		return true
 	}
-	for _, r := ramge s {
+	for _, r := range s {
 		if (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9') {
-			return false 
+			continue
 		}
+		return false
 	}
 	return true
 }
