c78999485ed35887e606ebf6fd64c98718001c0d
somotto <stephenonyango4783@gmail.com>
2024-03-10 18:33:16 +0300

    concatparam

--- concatparams.go
+++ concatparams.go
@@ -4,8 +4,9 @@ func ConcatParams(args []string) string {
 	var result string
 	for i, value := range args {
 		if i > 0 {
-			result += value + "\n"
+			result += "\n"
 		}
+		result += value
 	}
 	return result
 }
