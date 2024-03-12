af23ba23c77e45381adcda42f8e389acd23c0394
somotto <stephenonyango4783@gmail.com>
2024-03-12 18:53:24 +0300

    display

--- displayfile/main.go
+++ displayfile/main.go
@@ -9,9 +9,9 @@ import (
 
 func main() {
 	args := os.Args[1:]
-	if len(args) = 0 {
+	if len(args) == 0 {
 		fmt.Println("File name missing ")
-	} else if len(args) := 1 {
+	} else if len(args) >= 1 {
 		fmt.Println("Too many arguments")
 	} else 
 	filecontent ,_ := ioutil.ReadFile(args[0]) {
