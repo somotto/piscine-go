345dc3bc98922b72e47184e0d3891e38ddf3c999
somotto <stephenonyango4783@gmail.com>
2024-03-12 15:49:15 +0300

    booln

---
+++ boolean/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+const EvenMsg string = "I have an even number of arguments"
+const OddMsg string = "I have an odd number of arguments"
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+func even(nbr int) bool {
+	if nbr%2 == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func isEven(nbr int) bool {
+	if even(nbr) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	lengthOfArg := len(os.Args[1:])
+	if isEven(lengthOfArg) {
+		printStr(EvenMsg)
+	} else {
+		printStr(OddMsg)
+	}
+}
