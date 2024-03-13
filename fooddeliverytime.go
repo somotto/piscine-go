06f3ea1a398170655f9dcbcd92b871f19d25c93c
somotto <stephenonyango4783@gmail.com>
2024-03-13 13:53:14 +0300

    food

--- fooddeliverytime.go
+++ fooddeliverytime.go
@@ -2,22 +2,22 @@ package piscine
 
 type food struct {
 	preptime int
-  } 
-  var menu = map[string]food{
-	"burger": {preptime: 15},
-	"chips": {preptime: 10},
+}
+
+var menu = map[string]food{
+	"burger":  {preptime: 15},
+	"chips":   {preptime: 10},
 	"nuggets": {preptime: 12},
-  }
-  
-  func FoodDeliveryTime(order string) int {
-	totalTime := 0
-	items := split(order, ",")
-	for _, item := range items {
-		orderItem,ok := menu[trimSpace(item)]
-		if !ok {
-			return 0, errorf("404: Menu item \"%s\" notfound",item)
+}
+
+func FoodDeliveryTime(order string) int {
+	totalprepTime := 0
+	for _, item := range order {
+		foodItem := string(item)
+		if menu[foodItem].preptime == 0 {
+			return -1
 		}
-		totalTime +=
+		totalprepTime += menu[foodItem].preptime
 	}
-  }
-  
+	return totalprepTime
+}
