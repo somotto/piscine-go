26e21e4f81ea822a7a4ba6d110cad8b71c51e714
somotto <stephenonyango4783@gmail.com>
2024-03-13 23:39:22 +0300

    descendappendrange

--- descendappendrange.go
+++ descendappendrange.go
@@ -1,10 +1,10 @@
 package piscine
 
 func DescendAppendRange(max, min int) []int {
-	if min >= max {
-		return nil
-	}
 	result := []int{}
+	if min > max {
+		return []int{}
+	}
 	for i := max; i > min; i-- {
 		result = append(result, i)
 	}
