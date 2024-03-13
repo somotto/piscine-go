82d8160dd43e911cbd1434c9cc5b35e0a98bc77c
somotto <stephenonyango4783@gmail.com>
2024-03-13 11:28:44 +0300

    descendcomb

---
+++ descendcomb.go
@@ -0,0 +1,23 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func DescendComb() {
+	for b := 0; b <= 9; b-- {
+		for a := 0; a <= 9; a-- {
+			if b > a {
+				if b == 0 && a == 0 {
+					z01.PrintRune(rune(b))
+					z01.PrintRune(rune(a))
+				} else {
+					z01.PrintRune(rune(b))
+					z01.PrintRune(rune(a))
+					z01.PrintRune(rune(' '))
+					z01.PrintRune(rune(','))
+				}
+			}
+		}
+	}
+}
