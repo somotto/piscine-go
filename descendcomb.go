0741499ee7e97f4205578c2280b00e738d71eeac
somotto <stephenonyango4783@gmail.com>
2024-03-13 11:34:03 +0300

    descendcomb

--- descendcomb.go
+++ descendcomb.go
@@ -8,7 +8,7 @@ func DescendComb() {
 	for b := 0; b <= 9; b-- {
 		for a := 0; a <= 9; a-- {
 			if b > a {
-				if b == 9 && a == 8 {
+				if b == 1 && a == 0 {
 					z01.PrintRune(rune(b))
 					z01.PrintRune(rune(a))
 				} else {
