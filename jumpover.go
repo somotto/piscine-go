338a056987bf48cfe609fc0a7ed0d5268437d131
somotto <stephenonyango4783@gmail.com>
2024-03-13 19:51:15 +0300

    jumpover

---
+++ jumpover.go
@@ -0,0 +1,14 @@
+package piscine
+
+func JumpOver(str string) string {
+	if str == "" || len(str) < 3 {
+		return "\n"
+	}
+	var result string
+	for i, char := range str {
+		if (i+1)%3 == 0 {
+			result += string(char)
+		}
+	}
+	return result
+}
