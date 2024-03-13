912b3daca00b5a002bc4695e887ce74ba80e57e9
somotto <stephenonyango4783@gmail.com>
2024-03-13 20:38:32 +0300

    stringtointslice

--- stringtointslice.go
+++ stringtointslice.go
@@ -1,18 +1,9 @@
 package piscine
 
 func StringToIntSlice(str string) []int {
-	res := []int{}
-	var num int
+	var result []int
 	for _, char := range str {
-		if '0' <= char && char <= '9' {
-			num = num*10 + int(char-'0')
-		} else if num > 0 {
-			res = append(res, num)
-			num = 0
-		}
+		result = append(result, int(char))
 	}
-	if num > 0 {
-		res = append(res, num)
-	}
-	return res
+	return result
 }
