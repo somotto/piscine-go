501a4bb207992c169742ce9739864b53c9294201
somotto <stephenonyango4783@gmail.com>
2024-03-13 15:29:15 +0300

    abort

--- abort.go
+++ abort.go
@@ -1,6 +1,17 @@
 package piscine
 
 func Abort(a, b, c, d, e int) int {
-	numbers := []int{a, b, c, d, e}
-	return numbers[2]
+	nums := []int{a, b, c, d, e}
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] > nums[j] {
+				nums[i], nums[j] = nums[j], nums[i]
+			}
+		}
+	}
+	mid := len(nums) / 2
+	if len(nums)%2 == 0 {
+		return (nums[mid-1] + nums[mid]) / 2
+	}
+	return nums[mid]
 }
