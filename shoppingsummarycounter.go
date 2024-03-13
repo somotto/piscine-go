d62e98a03603d9a2d28b807325864f8fc51773b2
somotto <stephenonyango4783@gmail.com>
2024-03-13 16:30:41 +0300

    shoppingsummarycounter

---
+++ shoppingsummarycounter.go
@@ -0,0 +1,20 @@
+package piscine
+
+func ShoppingSummaryCounter(str string) map[string]int {
+	summary := make(map[string]int)
+	word := ""
+	for _, char := range str {
+		if char == ' ' || char == '\t' || char == '\n' {
+			if word != "" {
+				summary[word] = summary[word] + 1
+			}
+			word = ""
+		} else {
+			word += string(char)
+		}
+	}
+	if word != "" {
+		summary[word] = summary[word] + 1
+	}
+	return summary
+}
