1b469b2e28f1784e291dcbfa3a11c9c90aa01b0d
somotto <stephenonyango4783@gmail.com>
2024-03-14 06:58:54 +0300

    fixthemain

---
+++ fixthemain/main.go
@@ -0,0 +1,51 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+const (
+	OPEN  = true
+	CLOSE = false
+)
+
+type Door struct {
+	state bool
+}
+
+func PrintStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+}
+
+func OpenDoor(ptrDoor *Door) {
+	PrintStr("Door Opening...")
+	ptrDoor.state = OPEN
+}
+
+func CloseDoor(ptrDoor *Door) bool {
+	PrintStr("Door Closing...")
+	ptrDoor.state = CLOSE
+	return true
+}
+
+func IsDoorOpen(Door Door) bool {
+	PrintStr("is the Door opened ?")
+	return Door.state == OPEN
+}
+
+func IsDoorClose(ptrDoor *Door) bool {
+	PrintStr("is the Door closed ?")
+	return ptrDoor.state == CLOSE
+}
+
+func main() {
+	door := &Door{}
+
+	OpenDoor(door)
+	if IsDoorClose(door) {
+		OpenDoor(door)
+	}
+	if door.state == OPEN {
+		CloseDoor(door)
+	}
+}
