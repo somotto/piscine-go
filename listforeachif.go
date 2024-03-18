295943c583f7a1ed9bb2c30c2dcff1c947ca22ce
somotto <stephenonyango4783@gmail.com>
2024-03-19 02:58:33 +0300

    listforeachif

---
+++ listforeachif.go
@@ -0,0 +1,27 @@
+package piscine
+
+func IsPositiveNode(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return node.Data.(int) > 0
+	default:
+		return false
+	}
+}
+
+func IsAlNode(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return false
+	default:
+		return true
+	}
+}
+
+func ListForEachIf(l *List, f func(*NodeL), cond func(*NodeL) bool) {
+	for current := l.Head; current != nil; current = current.Next {
+		if cond(current) {
+			f(current)
+		}
+	}
+}
