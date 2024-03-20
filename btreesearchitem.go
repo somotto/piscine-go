3367d9ccf69707f28961a8c5d06e356e5ebe76de
somotto <stephenonyango4783@gmail.com>
2024-03-20 15:50:43 +0300

    btreesearchitem

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -4,14 +4,12 @@ func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 	if root == nil {
 		return root
 	}
-	if root.Data == elem {
+	if elem == root.Data {
 		return root
 	}
-	if root.Left != nil {
+	if elem < root.Data {
 		return BTreeSearchItem(root.Left, elem)
-	}
-	if root.Right != nil {
+	} else {
 		return BTreeSearchItem(root.Right, elem)
 	}
-	return nil
 }
