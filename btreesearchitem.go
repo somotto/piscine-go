7d480a4d6062f1533a9e6f6989955a6699728857
somotto <stephenonyango4783@gmail.com>
2024-03-20 18:42:30 +0300

    btreesearchitem

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -1,5 +1,12 @@
 package piscine
 
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
+	if root == nil || root.Data == elem {
+		return root
+	}
+	leftResult := BTreeSearchItem(root.Left, elem)
+	if leftResult != nil {
+		return leftResult
+	}
 	return BTreeSearchItem(root.Right, elem)
 }
