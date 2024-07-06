6fccad468bf0c6c7dbc6e9a84f97bf2a42972bd4
somotto <stephenonyango4783@gmail.com>
2024-07-06 17:10:07 +0300

    feat: add function that multipies,sum and divide two ints

---
+++ reduceint/reduceint.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"strconv"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	mul := func(acc int, cur int) int {
+		return acc * cur
+	}
+	sum := func(acc int, cur int) int {
+		return acc + cur
+	}
+	div := func(acc int, cur int) int {
+		return acc / cur
+	}
+	as := []int{500, 2}
+	ReduceInt(as, mul)
+	ReduceInt(as, sum)
+	ReduceInt(as, div)
+}
+
+func ReduceInt(a []int, f func(int, int) int) {
+	result := a[0]
+	for i := 1; i < len(a); i++ {
+		result = f(result, a[i])
+	}
+	str := strconv.Itoa(result)
+	for _, ch := range str {
+		z01.PrintRune(ch)
+	}
+	z01.PrintRune('\n')
+}
