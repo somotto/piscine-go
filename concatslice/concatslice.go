f880f9463981adfdd18f82f37b6a6eb43b706622
somotto <stephenonyango4783@gmail.com>
2024-07-06 18:01:00 +0300

    feat: add two slices

---
+++ concatslice/concatslice.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func ConcatSlice(slice1, slice2 []int) []int {
+    result := make([]int, len(slice1) + len(slice2))
+    copy(result, slice1)
+    copy(result[len(slice1):], slice2)
+    return result
+}
+
+
+func main() {
+	fmt.Println(ConcatSlice([]int{1, 2, 3}, []int{4, 5, 6}))
+	fmt.Println(ConcatSlice([]int{}, []int{4, 5, 6, 7, 8, 9}))
+	fmt.Println(ConcatSlice([]int{1, 2, 3}, []int{}))
+}
