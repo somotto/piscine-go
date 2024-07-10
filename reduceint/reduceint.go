2c57809ca296e0a3d68fa0c645df106b78421ab1
somotto <stephenonyango4783@gmail.com>
2024-07-10 21:17:56 +0300

    handle error when slice is empty

--- reduceint/reduceint.go
+++ reduceint/reduceint.go
@@ -16,13 +16,16 @@ func main() {
 	div := func(acc int, cur int) int {
 		return acc / cur
 	}
-	as := []int{500, 2}
+	as := []int{}
 	ReduceInt(as, mul)
 	ReduceInt(as, sum)
 	ReduceInt(as, div)
 }
 
 func ReduceInt(a []int, f func(int, int) int) {
+	if len(a) == 0 {
+		return
+	}
 	result := a[0]
 	for i := 1; i < len(a); i++ {
 		result = f(result, a[i])
