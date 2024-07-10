3fe719c08f5aa5983a9dcf3ef970b849fe580469
somotto <stephenonyango4783@gmail.com>
2024-07-10 23:01:36 +0300

    add init displayfirstparam

---
+++ dispalyfirstparam/displyfstpar.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+
+
+func main() {
+	if len(os.Args[1:]) == 0 {
+		return
+	}
+	args := os.Args[1]
+	for _, ch := range args {
+		z01.PrintRune(ch)
+	}
+	z01.PrintRune('\n')
+}
