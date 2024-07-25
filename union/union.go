5932bea00736a181c857d7873e2c23b61b577792
somotto <stephenonyango4783@gmail.com>
2024-07-25 19:13:23 +0300

    testtrain

--- union/union.go
+++ union/union.go
@@ -7,22 +7,23 @@ import (
 )
 
 func main() {
-    args := os.Args[1:]
-    if len(args) != 2 {
-        z01.PrintRune('\n')
-        return
-    }
+	args := os.Args[1:]
+	if len(args) != 2 {
+		z01.PrintRune('\n')
+		return
+	}
 
-    seen := make(map[rune]bool)
-    
-    for _, str := range args {
-        for _, char := range str {
-            if !seen[char] {
-                z01.PrintRune(char)
-                seen[char] = true
-            }
-        }
-    }
-    
-    z01.PrintRune('\n')
+	str1 := args[0]
+	str2 := args[1]
+
+	seen := make(map[rune]bool)
+
+	for _, char := range str1 + str2 {
+		if !seen[char] {
+			z01.PrintRune(char)
+			seen[char] = true
+		}
+	}
+
+	z01.PrintRune('\n')
 }
