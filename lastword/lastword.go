393d6ecc2b49edf65272b3f886bcb214a280cf96
somotto <stephenonyango4783@gmail.com>
2024-07-27 12:10:00 +0300

    update lastword

--- lastword/lastword.go
+++ lastword/lastword.go
@@ -1,49 +1,41 @@
 package main
 
 import (
-	"github.com/01-edu/z01"
+	"fmt"
 )
 
-func LastWord(s string) {
-    lastWordStart := -1
-    wordEnd := -1
-
-    // Find the last non-space character
-    for i := len(s) - 1; i >= 0; i-- {
-        if s[i] != ' ' {
-            wordEnd = i
-            break
-        }
-    }
-
-    // If no non-space character found, return empty line
-    if wordEnd == -1 {
-        z01.PrintRune('\n')
-        return
-    }
-
-    // Find the start of the last word
-    for i := wordEnd; i >= 0; i-- {
-        if s[i] == ' ' {
-            lastWordStart = i + 1
-            break
-        }
-    }
-
-    // If no space found, the whole string is the last word
-    if lastWordStart == -1 {
-        lastWordStart = 0
-    }
-
-    // Print the last word
-    for i := lastWordStart; i <= wordEnd; i++ {
-        z01.PrintRune(rune(s[i]))
-    }
-    z01.PrintRune('\n')
+func main() {
+	fmt.Print(LastWord("this        ...       is sparta, then again, maybe    not"))
+	fmt.Print(LastWord(" lorem,ipsum "))
+	fmt.Print(LastWord(" "))
 }
 
-func main() {
-    LastWord("this        ...       is sparta, then again, maybe    not")
-    LastWord(" lorem,ipsum ")
-    LastWord(" ")
+func LastWord(s string) string {
+    // Trim trailing spaces
+    s = trimTrailingSpaces(s)
+
+    // If the string is empty after trimming, return a newline
+    if s == "" {
+        return "\n"
+    }
+
+    // Find the last word
+    lastSpaceIndex := -1
+    for i := len(s) - 1; i >= 0; i-- {
+        if s[i] == ' ' {
+            lastSpaceIndex = i
+            break
+        }
+    }
+
+    // Return the last word followed by a newline
+    return s[lastSpaceIndex+1:] + "\n"
+}
+
+func trimTrailingSpaces(s string) string {
+    end := len(s) - 1
+    for end >= 0 && s[end] == ' ' {
+        end--
+    }
+    return s[:end+1]
 }
