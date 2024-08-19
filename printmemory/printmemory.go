65acdbc000bf463fced8764c411f30c0eab07e68
somotto <stephenonyango4783@gmail.com>
2024-08-19 23:18:17 +0300

    changes

--- printmemory/printmemory.go
+++ printmemory/printmemory.go
@@ -1,33 +1,47 @@
 package main
 
-import "github.com/01-edu/z01"
-
-func PrintMemory(arr [10]byte) {
-	hexDigits := "0123456789abcdef"
-
-	// Print the hexadecimal representation in lines of 4 bytes each
-	for i := 0; i < len(arr); i += 4 {
-		for j := i; j < i+4 && j < len(arr); j++ {
-			z01.PrintRune(rune(hexDigits[arr[j]>>4]))
-			z01.PrintRune(rune(hexDigits[arr[j]&0x0F]))
-			if j != i+3 && j != len(arr)-1 {
-				z01.PrintRune(' ')
-			}
-		}
-		z01.PrintRune('\n')
-	}
-
-	 // Print the ASCII representation of all 10 bytes on a single line
-	 for i := 0; i < len(arr); i++ {
-        if arr[i] >= 32 && arr[i] <= 126 {
-            z01.PrintRune(rune(arr[i]))
-        } else {
-            z01.PrintRune('.')
-        }
-    }
-    z01.PrintRune('\n')
-}
+import (
+	"github.com/01-edu/z01"
+)
 
 func main() {
 	PrintMemory([10]byte{'h', 'e', 'l', 'l', 'o', 16, 21, '*'})
+}
+
+func PrintMemory(arr [10]byte) {
+	for i, v := range arr {
+		hex := hextodec(int(v))
+		if len(hex) == 1 {
+			z01.PrintRune('0')
+		}
+		for _, ch := range hex {
+			z01.PrintRune(rune(ch))
+		}
+		if i == 3 || i == 7 || i == 9 {
+			z01.PrintRune('\n')
+		} else if i != 9 {
+			z01.PrintRune(' ')
+		}
+	}
+	for _, v := range arr {
+		if v >= 32 && v <= 126 {
+			z01.PrintRune(rune(v))
+		} else {
+			z01.PrintRune('.')
+		}
+	}
+	z01.PrintRune('\n')
+}
+
+func hextodec(dec int) string {
+	if dec == 0 {
+		return "00"
+	}
+	s := "0123456789abcdef"
+	s1 := ""
+	for dec > 0 {
+		s1 = string(s[dec%16]) + s1
+		dec /= 16
+	}
+	return s1
 }
