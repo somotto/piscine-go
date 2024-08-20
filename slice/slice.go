3081ee403c1b8e9b20737f7648633523d43921e2
somotto <stephenonyango4783@gmail.com>
2024-08-21 00:40:46 +0300

    update2

--- slice/slice.go
+++ slice/slice.go
@@ -11,47 +11,34 @@ func main() {
 	fmt.Printf("%#v\n", Slice(a, -3))
 	fmt.Printf("%#v\n", Slice(a, -2, -1))
 	fmt.Printf("%#v\n", Slice(a, 2, 0))
+	fmt.Printf("%#v\n", Slice(a, 2, 1, 9, 8, 7))
+
 }
 
 func Slice(a []string, nbrs ...int) []string {
-	if len(nbrs) == 0 {
+	if len(nbrs)== 0{
 		return nil
 	}
-
-	start := 0
+	start := nbrs[0]
 	end := len(a)
 
-	// Handle the start index
-	if len(nbrs) >= 1 {
-		start = nbrs[0]
-		if start < 0 {
-			start = len(a) + start
-		}
-		if start < 0 {
-			start = 0
-		}
-		if start > len(a) {
-			start = len(a)
-		}
+	if start < 0{
+		start = len(a)+ start
 	}
 
-	// Handle the end index
-	if len(nbrs) >= 2 {
-		end = nbrs[1]
-		if end < 0 {
-			end = len(a) + end
+	if len(nbrs) >1{
+		end = nbrs[len(nbrs)-1]
+
+		if end < 0{
+			end = len(a)+ end
 		}
-		if end < 0 {
-			end = 0
-		}
-		if end > len(a) {
+		if end > len(a){
 			end = len(a)
 		}
-		if end < start {
+
+		if start < 0 || start > end || start >= len(a){
 			return nil
 		}
 	}
-
-	// Return the slice based on the calculated start and end indices
-	return a[start:end]
+	return a[start : end]
 }
