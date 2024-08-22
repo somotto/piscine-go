9e10cf88b0fc562f8384304345a3d44e46e5b2ff
somotto <stephenonyango4783@gmail.com>
2024-08-22 18:00:23 +0300

    feat: slice2

--- slice/slice.go
+++ slice/slice.go
@@ -1,44 +1,45 @@
 package main
 
-import (
-	"fmt"
-)
-
-func main() {
-	a := []string{"coding", "algorithm", "ascii", "package", "golang"}
-	fmt.Printf("%#v\n", Slice(a, 1))
-	fmt.Printf("%#v\n", Slice(a, 2, 4))
-	fmt.Printf("%#v\n", Slice(a, -3))
-	fmt.Printf("%#v\n", Slice(a, -2, -1))
-	fmt.Printf("%#v\n", Slice(a, 2, 0))
-	fmt.Printf("%#v\n", Slice(a, 2, 1, 9, 8, 7))
-
-}
 
 func Slice(a []string, nbrs ...int) []string {
-	if len(nbrs)== 0{
+	if len(nbrs) == 0 {
 		return nil
 	}
-	start := nbrs[0]
+
+	start := 0
 	end := len(a)
 
-	if start < 0{
-		start = len(a)+ start
+	// Handle the start index
+	if len(nbrs) >= 1 {
+		start = nbrs[0]
+		if start < 0 {
+			start = len(a) + start
+		}
+		if start < 0 {
+			start = 0
+		}
+		if start > len(a) {
+			start = len(a)
+		}
 	}
 
-	if len(nbrs) >1{
-		end = nbrs[len(nbrs)-1]
-
-		if end < 0{
-			end = len(a)+ end
+	// Handle the end index
+	if len(nbrs) >= 2 {
+		end = nbrs[1]
+		if end < 0 {
+			end = len(a) + end
 		}
-		if end > len(a){
+		if end < 0 {
+			end = 0
+		}
+		if end > len(a) {
 			end = len(a)
 		}
-
-		if start < 0 || start > end || start >= len(a){
+		if end < start {
 			return nil
 		}
 	}
-	return a[start : end]
+
+	// Return the slice based on the calculated start and end indices
+	return a[start:end]
 }
