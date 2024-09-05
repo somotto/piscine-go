0c8775f380c747e5a2b9dd574143a9a70c38e85f
somotto <stephenonyango4783@gmail.com>
2024-09-05 18:26:59 +0300

    updateword

--- wordflip/wordflip.go
+++ wordflip/wordflip.go
@@ -4,61 +4,31 @@ import "fmt"
 
 // WordFlip reverses the order of words in a string
 func WordFlip(str string) string {
-	// Step 1: Trim leading and trailing spaces
-	start := 0
-	end := len(str) - 1
-
-	// Find the start index of the first non-space character
-	for start <= end && (str[start] == ' ' || str[start] == '\t') {
-		start++
+	if str == "" {
+		return "Invalid Output" + "\n"
 	}
-
-	// Find the end index of the last non-space character
-	for end >= start && (str[end] == ' ' || str[end] == '\t') {
-		end--
+	result := ""
+	slice := []string{}
+	for _, ch := range str {
+		if ch != ' ' {
+			result += string(ch)
+		} else if result != "" {
+			slice = append(slice, result)
+			result = ""
+		}
 	}
-
-	// If the resulting string is empty
-	if start > end {
-		return "Invalid Output\n"
+	if result != "" {
+		slice = append(slice, result)
 	}
-
-	// Step 2: Normalize multiple spaces between words
-	trimmedStr := ""
-	inWord := false
-	for i := start; i <= end; i++ {
-		if str[i] == ' ' || str[i] == '\t' {
-			if inWord {
-				trimmedStr += " "
-				inWord = false
-			}
+	wordflipped := ""
+	for i := len(slice)-1; i>= 0; i-- {
+		if i != 0 {
+			wordflipped += slice[i] + " "
 		} else {
-			trimmedStr += string(str[i])
-			inWord = true
+			wordflipped += slice[i]
 		}
 	}
-
-	// Step 3: Reverse the order of words
-	words := []string{}
-	start = 0
-	for i := 0; i <= len(trimmedStr); i++ {
-		if i == len(trimmedStr) || trimmedStr[i] == ' ' {
-			if start < i {
-				words = append(words, trimmedStr[start:i])
-			}
-			start = i + 1
-		}
-	}
-
-	reversedStr := ""
-	for i := len(words) - 1; i >= 0; i-- {
-		if i < len(words)-1 {
-			reversedStr += " "
-		}
-		reversedStr += words[i]
-	}
-
-	return reversedStr + "\n"
+	return wordflipped + "\n"
 }
 
 func main() {
